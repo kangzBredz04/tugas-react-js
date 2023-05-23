fd41c4ec03d11403dfd39d22be45e6f0318b15d1
Wahyu <wabredz1234@gmail.com>
2023-05-23 08:12:46 +0700

    Menambahkan function fungsi1

--- pertemuan1.js
+++ pertemuan1.js
@@ -1,5 +1,14 @@
+// Anonymous function
 const helloWorld = function () {
   console.log("Hello World !!!");
 };
 
 helloWorld();
+
+function fungsi1() {
+  console.log("Ini fungsi1");
+}
+
+fungsi1(function () {
+  console.log("Memanggil fungsi1 dengan argumen berupa Anonymous function");
+});
